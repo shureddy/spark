26f294d0b65269f90458de1e512bd0b066b0bd4c
yaswanthreddy <yaswanthreddy@yaswanths-mbp-2.lan1>
2019-04-02 20:03:59 -0500

    spark cache

--- spark/src/main/scala/com/shu/spark/func_spark.scala
+++ spark/src/main/scala/com/shu/spark/func_spark.scala
@@ -27,9 +27,9 @@ object func_spark {
     println("Schema in different patterns")
     df_udf.printSchema()
     println(df_udf.schema.json)
-    val sch_seq=df_udf.schema.fields.toSeq //print all the fields with data types associated with it.
+    val sch_seq = df_udf.schema.fields.toSeq //print all the fields with data types associated with it.
     println(sch_seq)
-    val sch_names=df_udf.schema.names.toSeq //print only the field names.
+    val sch_names = df_udf.schema.names.toSeq //print only the field names.
     println(sch_names)
     sch_names.foreach(println)
   }
